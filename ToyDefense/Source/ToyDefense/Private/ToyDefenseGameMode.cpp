b750931ea27ebe65a98865a072e4f0d29fd812de
ezeigler <ezeigler22@yahoo.com>
2014-10-17 23:12:58 -0400

    ToyDefense
    Game for Game jam

---
+++ ToyDefense/Source/ToyDefense/Private/ToyDefenseGameMode.cpp
@@ -0,0 +1,12 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#include "ToyDefense.h"
+#include "ToyDefenseGameMode.h"
+#include "ToyDefensePawn.h"
+
+AToyDefenseGameMode::AToyDefenseGameMode(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+	// set default pawn class to our flying pawn
+	DefaultPawnClass = AToyDefensePawn::StaticClass();
+}
---
+++ ToyDefense/Source/ToyDefense/Public/ToyDefenseGameMode.h
@@ -0,0 +1,14 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "ToyDefenseGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AToyDefenseGameMode : public AGameMode
+{
+	GENERATED_UCLASS_BODY()
+
+};
+
+
+
